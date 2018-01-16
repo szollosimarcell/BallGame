af8737f7847d7e20f76a09bae68acd277dcb6a57
Marci <szllsi.marcell@gmail.com>
2018-01-16 04:56:09 +0100

    1.2- Zene, zenebutton, javadoc, unittest, kód esztétika javítása, többször létrehozható infoframe javítása

--- src/test/java/test/BallGameTest.java
+++ src/test/java/test/BallGameTest.java
@@ -5,41 +5,83 @@
  */
 package test;
 
+import baseclasses.Music;
+import baseclasses.Player;
+import control.Controller;
+import control.MainFrame;
+import frames.InfoFrame;
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import panels.MenuPanel;
+import panels.ResultPanel;
 
 /**
  *
  * @author Marci
  */
 public class BallGameTest {
-    
+
+    private final String MUSIC_PATH = "/music/mainMusic.mp3";
+    private final String PLAYER_NAME = "Marci";
+
+    private Controller controller;
+    private MainFrame mainFrame;
+    private MenuPanel menuPanel;
+    private ResultPanel resultPanel;
+    private InfoFrame infoFrame;
+    private Music music;
+    private Player player;
+
     public BallGameTest() {
     }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
+
     @Before
     public void setUp() {
+        mainFrame = new MainFrame();
+        menuPanel = mainFrame.getMenuPanel1();
+        resultPanel = mainFrame.getResultPanel1();
+        controller = new Controller(menuPanel, resultPanel, mainFrame);
     }
-    
+
     @After
     public void tearDown() {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    @Test
+    public void musicTest() {
+        music = new Music();
+        music.setPath(MUSIC_PATH);
+        assertFalse(music.isAlive());
+        music.start();
+        assertTrue(music.isAlive());
+    }
+    
+    @Test
+    public void infoTest() {
+        infoFrame = new InfoFrame(controller);
+        controller.setInfoPanel(infoFrame.getInfoPanel1());
+        assertTrue(infoFrame.isVisible());
+    }
+    
+    @Test
+    public void panelChangeTest() {
+        controller.showResult();
+        assertFalse(menuPanel.isVisible());
+        assertTrue(resultPanel.isVisible());
+        controller.showMainMenu();
+        assertFalse(resultPanel.isVisible());
+        assertTrue(menuPanel.isVisible());
+    }
+    
+    @Test
+    public void playerTest() {
+        player = new Player(PLAYER_NAME);
+        assertTrue(player != null);
+        player.earnPoint();
+        assertTrue(player.getPoints() == 1);
+        player.cleanPoint();
+        assertTrue(player.getPoints() == 0);
+    }
 }
